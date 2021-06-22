7b91ce3ab92b593c10fdcf444fcddbc8f9b16e1f
dsimp <55302995+dsimp@users.noreply.github.com>
2021-06-21 19:16:26 -0500

    navigation add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <h1>yoooooooo</h1>
+      <Navigation />
     </div>
   );
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#bio">Bio</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#project">Project</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#github">Github</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#linkedIn">LinkedIn</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
