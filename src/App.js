b8e3b6f9f2ec0bc7e7fd9e527ecd04ea056764a8
dsimp <55302995+dsimp@users.noreply.github.com>
2021-06-23 09:42:33 -0500

    implementing Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Navigation from "./Components/Navigation";
+import Bio from "./Components/Bio";
+import Project from "./Components/Project";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <div id="container">
+        <div id="bio">
+          <Bio />
+        </div>
+        <div id="project">
+          <Project />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,25 +2,32 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#bio">Bio</a>
-        </li>
-        <li>
-          {" "}
-          <a href="#project">Project</a>
-        </li>
-        <li>
-          {" "}
-          <a href="#github">Github</a>
-        </li>
-        <li>
-          {" "}
-          <a href="#linkedIn">LinkedIn</a>
-        </li>
-      </ul>
-    </nav>
+    <header className="heady">
+      <nav className="navy">
+        <div>
+          <h2>Djvon</h2>
+        </div>
+        <div className="navlist">
+          <ul>
+            <li>
+              <a href="#linkedin">LinkedIn</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#github">Github</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#project">Project</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#bio">Bio</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
