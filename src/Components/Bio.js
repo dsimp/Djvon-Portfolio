3abb87637e50b35ebcbc4173adf14e0a15798be8
dsimp <55302995+dsimp@users.noreply.github.com>
2021-06-23 21:39:45 -0500

    import bio and project

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Bio = () => {
-  return <h1> Bio </h1>;
+  return (
+    <Fragment>
+      <header> Bio </header>
+      <h1>Hi, my name is Djvon Simpson.</h1>
+      <p>
+        I've always had a love for the web, so much that it inspired me to learn
+        how to contribute.
+      </p>
+      <h2>Now, I'm a developer</h2>
+    </Fragment>
+  );
 };
 
 export default Bio;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Project = () => {
-  return <h1>Project</h1>;
+  return (
+    <Fragment>
+      <h1>Project</h1>
+      <img
+        src="https://media.giphy.com/media/DahK8I5UbqwOTE9mVw/giphy.gif"
+        alt="gif of app"
+      />
+      ;
+    </Fragment>
+  );
 };
 
 export default Project;
