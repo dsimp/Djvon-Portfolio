c984fcbb0bd027a757c43e42a3caf71f65ea6127
dsimp <55302995+dsimp@users.noreply.github.com>
2021-07-03 20:58:10 -0500

    hover component addition

---
+++ src/Components/Hover.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Hover = () => {
+  return (
+    <div>
+      <h1>What Does LinkedIn Have to Say About My Skills</h1>
+      <h3>Javascript</h3>
+      <h3>React</h3>
+      <h3>CSS</h3>
+    </div>
+  );
+};
