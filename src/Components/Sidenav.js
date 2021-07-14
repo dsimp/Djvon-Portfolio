96273d3eb955e67cc172384d6c0bccd31be2e69e
dsimp <55302995+dsimp@users.noreply.github.com>
2021-07-13 21:08:03 -0500

    finalizing media query

--- src/Components/Sidenav.js
+++ src/Components/Sidenav.js
@@ -1,40 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { GrLinkedin } from "react-icons/gr";
-import "./Sidenav.css";
 
 const Sidenav = () => {
+  const [a, setA] = useState("");
+
+  document.onclick = (e) => {
+    if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
+      setA("");
+    }
+  };
+
+  const changer = () => {
+    if (a === "") {
+      setA("active");
+    } else {
+      setA("");
+    }
+  };
   return (
-    <div className="sidenav">
-      <div id="toggle"></div>
-      <div id="sidebar">
-        <li>
-          <a href="https://github.com/dsimp">Djvon</a>
-        </li>
-        <li>
-          {" "}
-          <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin">
-            <GrLinkedin />
-          </a>
-        </li>
-        <li>
-          {" "}
-          <a href="https://github.com/dsimp">
-            <FaGithub />
-          </a>
-        </li>
-        <li>
-          <a href="#connect">Contact</a>
-        </li>
-        <li>
-          {" "}
-          <a href="#project">Project</a>
-        </li>
-        <li>
-          <a href="#bio"> Bio</a>
-        </li>
+    <nav id="sidenav">
+      <div id="toggle" className={a} onClick={changer}></div>
+      <div id="sidebar" className={a}>
+        <ul>
+          <li>
+            <a href="https://github.com/dsimp">Djvon</a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin">
+              <GrLinkedin />
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://github.com/dsimp">
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a href="#connect">Contact</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#bio"> Bio</a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 export default Sidenav;
